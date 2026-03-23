eac4ad396f62e9db64f08f4419492e54a3af344c
Ayşe Asena DAĞCI <aadagci@aselsan.com>
2026-03-23 15:06:55 +0300

    test(root): add use-arrow-clusters to vitest workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,4 +3,5 @@ import { defineWorkspace } from "vitest/config";
 export default defineWorkspace([
   "packages/arrow-supercluster",
   "packages/arrow-cluster-layer",
+  'packages/use-arrow-clusters'
 ]);
